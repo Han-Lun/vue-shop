90cabcc724c059e42b05ec9e43fb26172baaa87f
Han-Lun <924853978@qq.com>
2017-11-28 13:13:46 +0800

    finish

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div class="block">
+      <span class="demonstration">默认不区分颜色</span>
+      <el-rate v-model="value1"></el-rate>
+    </div>
+    <div class="block">
+      <span class="demonstration">区分颜色</span>
+      <el-rate
+        v-model="value2"
+        :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+      </el-rate>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      value1: null,
+      value2: null,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
